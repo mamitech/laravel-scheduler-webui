da9f687bd1f8f55538550771753f5a8524d60ebb
Arif <arif@mamiteam.com>
2024-05-12 22:43:05 +0700

    add warning about execution time

--- resources/views/dashboard/execute.blade.php
+++ resources/views/dashboard/execute.blade.php
@@ -5,6 +5,7 @@
         <input type="text" name="command" class="form-control" placeholder="command" aria-label="command" value="{{ session('command') ?? 'inspire' }}" />
         <button class="btn btn-outline-primary" type="submit">Execute</button>
     </div>
+    <div class="alert alert-danger">DO NOT execute command which take running time more than {{ $timeout }} seconds!</div>
 </form>
 <div id="console-output" class="p-3 {{ session('commandOutputIsError') ? 'text-danger': '' }}">
     @if (session('commandOutputMessage'))
